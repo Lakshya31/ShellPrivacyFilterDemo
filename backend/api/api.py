80d7b755880bde6de28ff9560d45cbed5765d56e
Lakshya31 <lakshya.secret@gmail.com>
2020-10-03 07:56:34 +0530

    Done with all UI functionalities

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,8 +1,29 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import time
+import numpy
 
-@app.route("/")
-def hello():
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/", methods=['GET'])
+def test():
     return "Hello World!"
 
-app.run()
+@app.route("/analyze", methods=['POST'])
+def analyze():
+    data = request.get_json()
+    text = data['text']
+    print(text)
+
+    index1 = numpy.random.randint(0,len(text)//4)
+    index2 = numpy.random.randint(len(text)//4,len(text)//2)
+    index3 = numpy.random.randint(len(text)//2,len(text)//4*3)
+    index4 = numpy.random.randint(len(text)//4*3,len(text))
+
+    reply = [
+        [index1, index2],
+        [index3, index4]
+    ]
+
+    return jsonify(reply)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
+//Import React Class
 import React from 'react';
+
+//Import Stylesheet
 import './App.css';
+
+//Importing Components
+import TextBox from './Components/TextBox'
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <div className="UpperBlue"></div>
+      <div className="Lower">
+        <div className="LeftPane"></div>
+        <TextBox/>
+      </div>
     </div>
   );
 }
+
 
 export default App;
