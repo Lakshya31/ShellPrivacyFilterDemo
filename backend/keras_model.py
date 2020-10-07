89f227b0b98d3de8b631510140252aceb0b3d1a6
shreyashkgupta <48386323+shreyashkgupta@users.noreply.github.com>
2020-10-07 20:12:21 +0530

    Update keras_model.py

--- backend/keras_model.py
+++ backend/keras_model.py
@@ -53,8 +53,8 @@ os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 class KerasModel():
     def __init__(self):
         self.categories = ["Safe", "Confidential"]
-        self.X = pickle.load(open(os.path.join(os.path.expanduser(), "data", "Image Dataset", "features.pickle"), "rb"))
-        self.y = pickle.load(open(os.path.join(os.path.expanduser(), "data", "Image Dataset", "labels.pickle"), "rb"))
+        self.X = pickle.load(open(os.path.join(os.path.expanduser("~"), "data", "Image Dataset", "features.pickle"), "rb"))
+        self.y = pickle.load(open(os.path.join(os.path.expanduser("~"), "data", "Image Dataset", "labels.pickle"), "rb"))
         
     def normalize(self):
         self.X = self.X/255.0
@@ -96,7 +96,7 @@ class KerasModel():
         self.history = self.model.fit(self.X, self.y, batch_size = 32, epochs = 50, validation_split = 0.1)
         
     def save_model(self):
-        self.model.save(os.path.join(os.path.expanduser(),"backend", "models", "keras_model"))
+        self.model.save(os.path.join(os.path.expanduser("~"),"backend", "models", "keras_model"))
         with open(os.path.join(os.path.expanduser(),"backend", "models", "model_history"), 'wb') as file_pi:
             pickle.dump(self.history.history, file_pi)
         
@@ -111,8 +111,8 @@ class KerasModel():
         plt.legend(['Train', 'Validation'], loc = 'upper left')
         
     def load_model(self):
-        self.model = load_model(os.path.join(os.path.expanduser(),"backend", "models","keras_model"))
-        self.history = pickle.load(open(os.path.join(os.path.expanduser(),"backend", "models","model_history"), "rb"))
+        self.model = load_model(os.path.join(os.path.expanduser("~"),"backend", "models","keras_model"))
+        self.history = pickle.load(open(os.path.join(os.path.expanduser("~"),"backend", "models","model_history"), "rb"))
         
     def transform_image(self, image_path):
         img_size = 50
