f7548b823d5f0c8b096f278d4c718912a9932b63
Lakshya31 <lakshya.secret@gmail.com>
2020-10-07 21:38:57 +0530

    Updates

---
+++ precache-manifest.0d046785faedd879b0518704bb54e914.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a2f5e8e08cfb2022b082dcce8b6b3ab",
+    "url": "/ShellPrivacyFilterDemo/index.html"
+  },
+  {
+    "revision": "d2b97bf595abfe19a985",
+    "url": "/ShellPrivacyFilterDemo/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6e012ccf57594ac282b7",
+    "url": "/ShellPrivacyFilterDemo/static/css/main.25f86e14.chunk.css"
+  },
+  {
+    "revision": "d2b97bf595abfe19a985",
+    "url": "/ShellPrivacyFilterDemo/static/js/2.ed739361.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/ShellPrivacyFilterDemo/static/js/2.ed739361.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e012ccf57594ac282b7",
+    "url": "/ShellPrivacyFilterDemo/static/js/main.bea11eda.chunk.js"
+  },
+  {
+    "revision": "fe514df5716b122cd5a1",
+    "url": "/ShellPrivacyFilterDemo/static/js/runtime-main.74f01554.js"
+  },
+  {
+    "revision": "7e131baf883770f3707c4622664ed4dd",
+    "url": "/ShellPrivacyFilterDemo/static/media/Close.7e131baf.png"
+  },
+  {
+    "revision": "b9d2124234e16167a56a599f3a1e95a5",
+    "url": "/ShellPrivacyFilterDemo/static/media/Mail.b9d21242.png"
+  },
+  {
+    "revision": "abeb16390d98bdc5eb99d0268cd33018",
+    "url": "/ShellPrivacyFilterDemo/static/media/Maximize.abeb1639.png"
+  },
+  {
+    "revision": "73a939d33913772cb76bffeef716acd8",
+    "url": "/ShellPrivacyFilterDemo/static/media/Minimize.73a939d3.png"
+  },
+  {
+    "revision": "1b7062a06635f3aa78bedc00c2ff24a7",
+    "url": "/ShellPrivacyFilterDemo/static/media/attach.1b7062a0.png"
+  },
+  {
+    "revision": "fe3fd73a0c07ebfeeaad891df163b5aa",
+    "url": "/ShellPrivacyFilterDemo/static/media/off.fe3fd73a.png"
+  },
+  {
+    "revision": "18a37a36d4b1091557b013f468ae7384",
+    "url": "/ShellPrivacyFilterDemo/static/media/on.18a37a36.png"
+  }
+]);
